62bb3b1c6a52caf9fb1e644a8996991c40ecb575
LinearGap <102157470+LinearGap@users.noreply.github.com>
2022-03-25 17:05:13 +0000

    progress made.

--- source/PyYTDL.py
+++ source/PyYTDL.py
@@ -1,20 +1,53 @@
+import argparse
+from ast import arg
+
 class PyYTDL():
     """
     Main entry and control point. This is what is called from the command
     line when the program is run.
     Usage: PyYTDL.py url
-    Optional:   [-a] Download audio only
-                [-h] Download the highest quality video available
-                [-o "filename"] Specify the output filename
-                [-S] Specify the prefered resolution to download
-                [-p] Open the file after downloading and converting
+    Optional:   [-a] Download audio only. Default = False
+                [-hq] Download the highest quality video available. Default = False
+                [-o "filename"] Specify the output filename. Default = Video Title
+                [-r] Specify the prefered resolution to download. Default = 1080p
+                [-p] Open the file after downloading and converting. Default = False
     """
     pass
 
     def __init__(self):
+        # The settings dict to be updated by __parse_args
+        self.__settings = {'audio Only':False, 'hq':False, 'filename':'', 'resolution':1080, 'open':False}
+        # The YouTube URL to download from
+        self.__YTURL = ""
+        self.__parse_args()
+
+    def __parse_args(self):
+        """
+        Parse command line arguments into the settings list
+        """
+        parser = argparse.ArgumentParser(description="Download and Convert YouTube videos.")
+        parser.add_argument('-a', '--audio', dest='a', action='store_true', default=False, help="Download audio track only, discarding any video.")
+        parser.add_argument('-hq', '--high_quality', dest='hq', action='store_true', default=False, help="Download the highest quality versions of the video and audio available.")
+        parser.add_argument('-o', '--output', dest='o', metavar="Filename", action='store', default="", type=str, help="Specify the output filename and path.")
+        parser.add_argument('-r', '--resolution', dest='r', metavar='Resolution', action='store', default=1080, type=int, help="Specify the preferred resolution to download at. Will select closest higher resolution.")
+        parser.add_argument('-p', '--open', dest='p', action='store_true', default=False, help="Open the produced file after download and conversion has occured.")
+
+        parser.add_argument('url', metavar='Youtube URL', action='store', type=str, help='The url of the YouTube video to download.')
+
+        args = parser.parse_args()
+        self.__settings['audio only'] = args.a
+        self.__settings['hq'] = args.hq
+        self.__settings['filename'] = args.o
+        self.__settings['resolution'] = args.r
+        self.__settings['open'] = args.p
+        self.__YTURL = args.url
+
+    def __check_url(self):
+        """
+        Check the supplied URL is a valid URL. Remove the video identifier and append back to
+        YouTube link to ensure safety.
+        """
         pass
-
-
 
 # ----------------------------------------------------------------------------------
 def main():
--- source/PyYTDL_Downloader.py
+++ source/PyYTDL_Downloader.py
@@ -1,30 +1,33 @@
 from logging import exception
+import os
 from pytube import YouTube as PYT
 
 class YTVid():
     """
     Object that will contain information about each available stream available
-    for the supplied urls
-    Tag = Specific identifier for that video stream
-    Mime_type = The multimedia type
-    Resolution = The resolution of the video
-    Bitrate = The video bitrate of the stream
-    AVType = What type of stream is this Audio/Video
-    Is_Progresive = Is this a progressive type stream that contains audio and video
-    Video_codec = What video codec is this stream encoded with
-    Audio_codec = What audio codec is this stream encoded with
-    Filesize = How big is the full stream
+    for the supplied urls\n
+    Tag = Specific identifier for that video stream\n
+    Mime_type = The multimedia type\n
+    Resolution = The resolution of the video\n
+    Bitrate = The video bitrate of the stream\n
+    AVType = What type of stream is this Audio/Video\n
+    Is_Progresive = Is this a progressive type stream that contains audio and video\n
+    Video_codec = What video codec is this stream encoded with\n
+    Audio_codec = What audio codec is this stream encoded with\n
+    Filesize = How big is the full stream\n
+    FPS = The frames per second of the stream
     """
     def __init__(self):
         self.tag = 0
         self.mime_type = ""
-        self.resolution = ""
+        self.resolution = 0
         self.bitrate = ""
         self.avtype = ""
         self.is_progressive = False
         self.video_codec = ""
         self.audio_codec = ""
         self.filesize = 0
+        self.fps = 0
 
 class PyYTDL_Downloader():
     """
@@ -38,12 +41,14 @@ class PyYTDL_Downloader():
     def set_URL(self, URL):
         """
         Set the URL to get video from. Will also check the URL
-        is a valid YT video link
+        is a valid YT video link.
         """
         self.__YTV = PYT(URL)
         try:
             self.__YTV.streams
+            return self.__YTV.title
         except:
+            ### Throw an exception here if the URL is invalid
             print('error')
 
     def get_streams_raw(self):
@@ -67,8 +72,39 @@ class PyYTDL_Downloader():
             vid.audio_codec = stream.video_codec
             vid.video_codec = stream.audio_codec
             vid.avtype = stream.type
-            vid.resolution = stream.resolution
+            if stream.resolution != None:
+                # Strip the p from the resolution and cast to an integer
+                vid.resolution = int(stream.resolution[:-1])
             vid.is_progressive = not stream.is_dash
             vid.filesize = stream._filesize
             YTStreams.append(vid)
         return YTStreams
+
+    def add_download_progress_callback(self, callback):
+        """
+        Add a callback function that will be called whenever donwload progress
+        is made
+        func: Callable[[Any, bytes, int], None]
+        """
+        self.__ytv.register_on_progress_callback(callback)
+
+    def download_stream(self, ytvid: YTVid, download_directory="", filename="") -> str:
+            """
+            The actual download function that downloads the actual stream into a file.
+            Returns: OS path filename of the downloaded stream
+            """
+            # Use current directory if the download directory is blank
+            if download_directory == "":
+                download_directory = os.getcwd()
+            # Use video title if filename is blank
+            if filename == "":
+                filename = self.__YTV.title
+
+            # Perform the actual download function
+            try:
+                file_path = self.__YTV.streams.get_by_itag(ytvid.tag).download(output_path= download_directory, filename= filename, filename_prefix= 'tmp__')
+            except:
+                print("Download Error")
+                file_path = ""
+            return file_path
+        
